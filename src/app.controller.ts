492a9eef83bebc5d2b0ab9e5896ec5ca82df6054
AndySakov <andysakov1958@gmail.com>
2023-01-17 13:38:15 +0100

    Integrated sentry successfully

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("test/sentry")
+  testSentry(): string {
+    return this.appService.testSentry();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
+import * as Sentry from '@sentry/node';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  testSentry(): string {
+    const transaction = Sentry.startTransaction({
+      op: "test",
+      name: "My First Test Transaction",
+    });
+
+    setTimeout(() => {
+      try {
+        throw new Error("Sentry Test");
+      } catch (e) {
+        Sentry.captureException(e);
+      } finally {
+        transaction.finish();
+      }
+    }, 99);
+    return "Test Complete!"
   }
 }
