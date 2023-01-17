7e71a43ce1569b485900680c7f571b6e24100709
AndySakov <andysakov1958@gmail.com>
2023-01-17 13:38:15 +0100

    Integrated sentry successfully

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
+import "@sentry/tracing";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(Sentry.Handlers.requestHandler());
   await app.listen(3000);
 }
 bootstrap();
