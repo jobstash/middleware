44b01984f64bec26c1354c7a45340ea518b4d74e
AndySakov <andysakov1958@gmail.com>
2023-01-20 20:30:13 +0100

    ✨ feat(main controller): sentry test
    Added test instrumentation for sentry

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import * as Sentry from "@sentry/node";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,24 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("test/sentry")
+  testSentry(): string {
+    const transaction = Sentry.startTransaction({
+      op: "test",
+      name: "My First Test Transaction",
+    });
+
+    setTimeout(() => {
+      try {
+        throw new Error("Sentry Test");
+      } catch (e) {
+        Sentry.captureException(e);
+      } finally {
+        transaction.finish();
+      }
+    }, 99);
+    return "Test Complete!";
   }
 }
