4a789a6b2295c08f0f62eb8e0aa8ddfa4a471942
AndySakov <andysakov1958@gmail.com>
2023-01-23 06:30:46 +0100

    feat(scaffolding): Setup graphql, neo4j, passport auth with jwt

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as Sentry from "@sentry/node";
 import "@sentry/tracing";
+import helmet from "helmet";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.use(Sentry.Handlers.requestHandler());
   app.use(Sentry.Handlers.tracingHandler());
   app.use(Sentry.Handlers.errorHandler());
+  app.use(helmet());
+  app.enableCors({
+    credentials: true,
+    methods: ["GET", "OPTIONS", "POST"],
+    origin: process.env.ALLOWED_ORIGINS?.split(",") ?? [],
+  });
   await app.listen(process.env.APP_PORT ?? 8080);
 }
 bootstrap();
