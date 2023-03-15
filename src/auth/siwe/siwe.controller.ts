ef9483d6363056f99297a968ed0f324717e39d5d
DuckDegen <duckdegen@gmail.com>
2023-03-15 13:56:14 +0700

    RIP286(SIWE)
    *Added siwe to middleware

--- src/auth/siwe/siwe.controller.ts
+++ src/auth/siwe/siwe.controller.ts
@@ -1,49 +1,117 @@
+// siwe.controller.ts
 import {
-  BadRequestException,
-  Body,
   Controller,
   Get,
   Post,
+  Req,
+  Res,
+  HttpStatus,
+  HttpException,
+  Body,
 } from "@nestjs/common";
-import {
-  ApiBadRequestResponse,
-  ApiInternalServerErrorResponse,
-  ApiOkResponse,
-} from "@nestjs/swagger";
-import { ResponseEntity } from "src/shared/entities";
+import { Request, Response } from "express";
+import { getIronSession, IronSession, IronSessionOptions } from "iron-session";
+import { BackendService } from "../../backend/backend.service";
+import { AuthService } from "../auth.service";
 import { VerifyMessageInput } from "../dto/verify-message.input";
-import { SiweService } from "./siwe.service";
+
+import { generateNonce, SiweMessage } from "siwe";
 
 @Controller("siwe")
 export class SiweController {
-  constructor(private readonly siweService: SiweService) {}
+  private sessionConfig: IronSessionOptions;
+  private backendService: BackendService;
+  private authService: AuthService;
+
+  constructor() {
+    this.sessionConfig = {
+      cookieName: process.env.COOKIE_NAME || "connectkit-next-siwe",
+      password: process.env.SESSION_SECRET,
+      cookieOptions: {
+        secure: process.env.NODE_ENV === "production",
+      },
+    };
+  }
+
+  private async getSession<
+    TSessionData extends Record<string, unknown> = Record<string, unknown>,
+  >(
+    req: Request,
+    res: Response,
+    sessionConfig: IronSessionOptions,
+  ): Promise<IronSession & TSessionData> {
+    const session = (await getIronSession(
+      req,
+      res,
+      sessionConfig,
+    )) as IronSession &
+      TSessionData & {
+        nonce?: string;
+        address?: string;
+        chainId?: number;
+      };
+    return session;
+  }
 
   @Get("nonce")
-  @ApiOkResponse({
-    description: "A nonce generated for the SIWE process on the client-side",
-  })
-  @ApiInternalServerErrorResponse({
-    description: "Something went wrong generating the nonce",
-  })
-  async getNonce(): Promise<string> {
-    return this.siweService.getNonce();
+  async getNonce(@Req() req: Request, @Res() res: Response): Promise<void> {
+    const session = await this.getSession(req, res, this.sessionConfig);
+    if (!session.nonce) {
+      session.nonce = generateNonce();
+      await session.save();
+    }
+    res.send(session.nonce);
+  }
+
+  @Get("session")
+  async getSessionData(
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<void> {
+    const { address, chainId } = await this.getSession(
+      req,
+      res,
+      this.sessionConfig,
+    );
+    res.send({ address, chainId });
+  }
+
+  @Get("logout")
+  async logout(@Req() req: Request, @Res() res: Response): Promise<void> {
+    const session = await this.getSession(req, res, this.sessionConfig);
+    session.destroy();
+    res.status(HttpStatus.OK).end();
   }
 
   @Post("verify")
-  @ApiOkResponse({
-    description: "An object containing the auth token for the created user",
-  })
-  @ApiBadRequestResponse({
-    description: "Something went wrong verifying the message",
-  })
-  async verifyMessage(
+  async verify(
+    @Req() req: Request,
+    @Res() res: Response,
     @Body() body: VerifyMessageInput,
-  ): Promise<ResponseEntity> {
-    const res = await this.siweService.verifyMessage(body);
-    if (res.success) {
-      return res;
-    } else {
-      throw new BadRequestException(res);
+  ): Promise<void> {
+    try {
+      const session = await this.getSession(req, res, this.sessionConfig);
+      const { message, signature } = body;
+      const siweMessage = new SiweMessage(message);
+      const fields = await siweMessage.validate(signature);
+
+      if (fields.nonce !== session.nonce) {
+        throw new HttpException(
+          "Invalid nonce.",
+          HttpStatus.UNPROCESSABLE_ENTITY,
+        );
+      }
+
+      this.backendService.createSIWEUser(siweMessage.address);
+
+      (session.token = this.authService.createToken(siweMessage.address)),
+        (session.address = fields.address);
+      session.chainId = fields.chainId;
+      await session.save();
+
+      res.status(HttpStatus.OK).end();
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 }
