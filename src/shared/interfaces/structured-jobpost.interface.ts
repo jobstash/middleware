4837fcfea4febdc9344d0c10241ed772d762fdfd
DuckDegen <duckdegen@gmail.com>
2023-03-24 12:09:26 +0700

    fix(interfaces): add new fields

--- src/shared/interfaces/structured-jobpost.interface.ts
+++ src/shared/interfaces/structured-jobpost.interface.ts
@@ -3,38 +3,64 @@ import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 export class StructuredJobpost {
   @ApiProperty()
   id: string;
+
   @ApiProperty()
   shortUUID: string;
+
   @ApiProperty()
   minSalaryRange: number;
+
   @ApiProperty()
   maxSalaryRange: number;
+
   @ApiProperty()
   role: string;
+
   @ApiProperty()
   seniority: string;
+
   @ApiProperty()
   team: string;
+
   @ApiProperty()
   benefits: string;
+
   @ApiProperty()
   culture: string;
+
   @ApiProperty()
-  hardSkills: string[];
+  salaryCurrency: string;
+
+  @ApiProperty()
+  paysInCrypto: boolean;
+
+  @ApiProperty()
+  offersTokenAllocation: boolean;
+
   @ApiProperty()
   jobApplyPageUrl: string;
+
   @ApiPropertyOptional()
   jobCommitment?: string;
+
   @ApiProperty()
   jobCreatedTimestamp: string;
+
   @ApiProperty()
   jobFoundTimestamp: string;
+
   @ApiProperty()
   jobPageUrl: string;
+
   @ApiProperty()
   jobLocation: string;
+
   @ApiProperty()
   jobTitle: string;
+
+  @ApiProperty()
+  aiDetectedTechnologies: string;
+
   @ApiProperty()
   extractedTimestamp: string;
 }
