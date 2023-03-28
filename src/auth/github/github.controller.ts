b4c88351948992660a563e1f78fcdbdb5a2a80bb
John Ballesteros <johnshift.dev@gmail.com>
2023-03-28 17:27:16 +0800

    fix: github-login role uppercase, tokenData destructure

--- src/auth/github/github.controller.ts
+++ src/auth/github/github.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Post, Body, Redirect } from "@nestjs/common";
 import { BackendService } from "../../backend/backend.service";
 import { AuthService } from "../auth.service";
 import { ConfigService } from "@nestjs/config";
-import { GithubConfig, User } from "src/shared/types";
+import { CheckWalletRoles, GithubConfig, User } from "src/shared/types";
 import { ApiExtraModels, ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
 import { UserService } from "../user/user.service";
 
@@ -74,29 +74,33 @@ export class GithubController {
     // Todo: is this ok? How would we update the user github token/data?
     // Todo: handle the case where the user has already logged in but now some data is different
     if (result.githubId === undefined) {
-      const { data: tokenData } = await axios.get(
+      const { data: tokenParamsString } = await axios.get(
         `https://github.com/login/oauth/access_token?client_id=${
-          role === "org"
+          role === CheckWalletRoles.ORG
             ? this.ghConfig.org.clientID
             : this.ghConfig.dev.clientID
         }&client_secret=${
-          role === "org"
+          role === CheckWalletRoles.ORG
             ? this.ghConfig.org.clientSecret
             : this.ghConfig.dev.clientSecret
         }&code=${code}`,
       );
+      // Note: tokenParamsString returns just a string like
+      // access_token=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&scope=read%3Aorg%2Cread%3Auser&token_type=bearer
+      const params = new URLSearchParams(tokenParamsString);
+      const accessToken = params.get("access_token");
       const { data: profileData } = await axios.get(
         "https://api.github.com/user",
         {
           headers: {
-            Authorization: `Bearer ${tokenData.access_token}`,
+            Authorization: `Bearer ${accessToken}`,
           },
         },
       );
 
       return this.backendService.addGithubInfoToUser({
-        githubAccessToken: tokenData.access_token,
-        githubRefreshToken: tokenData.refresh_token,
+        githubAccessToken: accessToken,
+        githubRefreshToken: "", // TODO: where do we get this? tokenData does not return this
         githubLogin: profileData.login,
         githubId: profileData.id,
         githubNodeId: profileData.node_id,
