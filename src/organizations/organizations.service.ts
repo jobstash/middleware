60a46c6332ce6ac668cd38c1dc31b6218081f054
Obafemi Teminife <andysakov1958@gmail.com>
2023-03-29 12:32:51 +0100

    feat(organizations): implemented search orgs
    RESOLVES RIP-335

--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -30,4 +30,30 @@ export class OrganizationsService {
         }),
       );
   }
+
+  async searchOrganizations(query: string): Promise<ShortOrg[]> {
+    return this.neo4jService
+      .read(
+        `
+        MATCH (o:Organization)
+        OPTIONAL MATCH (o)-[:HAS_JOBSITE]->(:Jobsite)-[:HAS_JOBPOST]->(jp:Jobpost)
+        OPTIONAL MATCH (jp)-[:HAS_STRUCTURED_JOBPOST]->(j:StructuredJobpost)
+        OPTIONAL MATCH (o)-[:HAS_FUNDING_ROUND]->(fr:FundingRound)
+        OPTIONAL MATCH (o)-[:HAS_PROJECT]->(p:Project)
+        OPTIONAL MATCH (j)-[:USES_TECHNOLOGY]->(t:Technology)
+        WITH o.orgId as id, o.logoUrl as logo, o.name as name, o.location as location, o.headCount as headCount, COUNT(DISTINCT p) as projectCount, COUNT(DISTINCT j) as jobCount, COLLECT(DISTINCT t) as technologies, fr
+        ORDER BY fr.date DESC
+        WITH id, name, logo, location, headCount, projectCount, jobCount, technologies, collect(fr)[0] as mrfr
+        WHERE name CONTAINS $query
+        RETURN { id: id, name: name, logo: logo, location: location, headCount: headCount, projectCount: projectCount, jobCount: jobCount, technologies: technologies, lastFundingAmount: mrfr.raisedAmount, lastFundingDate: mrfr.date } as res
+        `,
+        { query },
+      )
+      .then(res =>
+        res.records.map(record => {
+          const ent = new ShortOrgEntity(record.get("res")).getProperties();
+          return ent;
+        }),
+      );
+  }
 }
