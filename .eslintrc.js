f4c858dc2643eb16ce324e283580e8ca9b1f3275
Obafemi Teminife <andysakov1958@gmail.com>
2023-05-09 16:44:17 +0100

    fix(linter): temp fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "warn",
     "@typescript-eslint/explicit-module-boundary-types": "warn",
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/naming-convention": [
       "error",
       {
