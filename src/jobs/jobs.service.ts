c7b54afad24c58ad36ba39bcb5c0278a0f3ff28e
Obafemi Teminife <andysakov1958@gmail.com>
2023-05-16 20:51:11 +0100

    fix(jobs): refactored jobs list to return new jobs list result interface fixed bugs on jobs list upgraded nest-neo4j

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -194,7 +194,7 @@ export class JobsService {
               WITH p ORDER BY p.monthlyVolume DESC
               RETURN COLLECT(DISTINCT p)[0] as pf
             }
-            WITH o, j, pf, COLLECT(DISTINCT p) as projects, COLLECT(DISTINCT fr) as rounds, MAX(fr.date) as mrfr, COLLECT(DISTINCT i) as investors, COLLECT(DISTINCT t) AS tech, COLLECT(DISTINCT c) as cats, COLLECT(DISTINCT ch) as chains, COUNT(DISTINCT a) as auditCount, COUNT(DISTINCT h) as hackCount, COUNT(DISTINCT ch) as chainCount
+            WITH o, j, pf, COLLECT(DISTINCT p) as projects, COLLECT(DISTINCT fr) as rounds, MAX(fr.date) as mrfr, COLLECT(DISTINCT i) as investors, COLLECT(DISTINCT t) AS tech, COLLECT(DISTINCT c) as cats, COUNT(DISTINCT a) as auditCount, COUNT(DISTINCT h) as hackCount, COUNT(DISTINCT ch) as chainCount
             ${generatedFilters}
             WITH o, pf, j, tech, cats, auditCount, hackCount, chainCount, mrfr
             CALL {
@@ -245,6 +245,7 @@ export class JobsService {
             WITH count, COLLECT(DISTINCT results) as data
             RETURN { total: count, data: data } as res
         `.replace(/^\s*$(?:\r\n?|\n)/gm, "");
+    console.log(generatedQuery);
     return this.neo4jService
       .read(generatedQuery, {
         ...params,
