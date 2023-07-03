b7250ec7a264446724dcab00e3ac4a0cb216bb8b
Obafemi Teminife <andysakov1958@gmail.com>
2023-07-03 18:14:54 +0100

    feat(jobs): added dirty node checker for cache mgt

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -36,11 +36,35 @@ export class JobsService {
     private cacheManager: Cache,
   ) {}
 
+  async shouldClearCache(): Promise<boolean> {
+    return this.neo4jService
+      .read(
+        `
+          MATCH (node: DirtyNode)
+          RETURN node.dirty as isDirty
+      `.replace(/^\s*$(?:\r\n?|\n)/gm, ""),
+      )
+      .then(res => (res.records[0]?.get("isDirty") as boolean) ?? false)
+      .catch(err => {
+        Sentry.withScope(scope => {
+          scope.setTags({
+            action: "db-call",
+            source: "jobs.service",
+          });
+          Sentry.captureException(err);
+        });
+        this.logger.error(`JobsService::shouldClearCache ${err.message}`);
+        return false;
+      });
+  }
+
   async getProjectsData(): Promise<Project[]> {
+    const isDirty = await this.shouldClearCache();
     const cachedProjectsString =
       (await this.cacheManager.get<string>("projects")) ?? "[]";
     const cachedProjects = JSON.parse(cachedProjectsString) as Project[];
     if (
+      isDirty &&
       cachedProjects !== null &&
       cachedProjects !== undefined &&
       cachedProjects.length !== 0
@@ -430,10 +454,12 @@ export class JobsService {
       limit: null,
       page: null,
     }).filter(x => x !== null);
+    const isDirty = await this.shouldClearCache();
     const cachedJobsString =
       (await this.cacheManager.get<string>("jobs")) ?? "[]";
     const cachedJobs = JSON.parse(cachedJobsString) as JobListResult[];
     if (
+      isDirty &&
       cachedJobs !== null &&
       cachedJobs !== undefined &&
       cachedJobs.length !== 0 &&
