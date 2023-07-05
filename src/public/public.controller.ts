488818465f6bdfd4ac6868808af67e731633ab60
Obafemi Teminife <andysakov1958@gmail.com>
2023-07-05 08:47:22 +0100

    feat(public): added swagger bearer auth support

--- src/public/public.controller.ts
+++ src/public/public.controller.ts
@@ -7,10 +7,12 @@ import {
 } from "@nestjs/common";
 import { PublicService } from "./public.service";
 import { ApiKeyGuard } from "src/auth/api-key.guard";
-import { ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
 import { JobListResult, PaginatedData } from "src/shared/interfaces";
 import { CustomLogger } from "src/shared/utils/custom-logger";
+import { AllJobsInput } from "./dto/all-jobs.input";
 
+@ApiBearerAuth()
 @Controller("public")
 export class PublicController {
   logger = new CustomLogger(PublicController.name);
@@ -43,10 +45,7 @@ export class PublicController {
   })
   async getAllJobs(
     @Query(new ValidationPipe({ transform: true }))
-    params: {
-      page: number;
-      limit: number;
-    },
+    params: AllJobsInput,
   ): Promise<PaginatedData<JobListResult>> {
     this.logger.log(`/public/all-jobs ${JSON.stringify(params)}`);
     return this.publicService.getAllJobsList(params);
