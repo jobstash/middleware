e4e1fe86bb5237c9e4b1b3b17ba4cc4b66d132f9
Obafemi Teminife <andysakov1958@gmail.com>
2023-07-10 14:21:17 +0100

    fix(orgs): fixed bugs in tests

--- src/organizations/organizations.controller.spec.ts
+++ src/organizations/organizations.controller.spec.ts
@@ -41,21 +41,10 @@ describe("OrganizationsController", () => {
       c => c.id,
       `Chain for Project ${project.id} for Org ${orgId}`,
     );
-    const hasDuplicateCategories = hasDuplicates(
-      project.categories,
-      c => c.id,
-      `Category for Project ${project.id} for Org ${orgId}`,
-    );
     expect(hasDuplicateAudits).toBe(false);
     expect(hasDuplicateHacks).toBe(false);
     expect(hasDuplicateChains).toBe(false);
-    expect(hasDuplicateCategories).toBe(false);
-    return (
-      hasDuplicateAudits &&
-      hasDuplicateHacks &&
-      hasDuplicateChains &&
-      hasDuplicateCategories
-    );
+    return hasDuplicateAudits && hasDuplicateHacks && hasDuplicateChains;
   };
 
   const olrHasArrayPropsDuplication = (
