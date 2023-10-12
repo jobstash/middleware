c85c61a95a61f251b920d8915ed25dad7af14a13
Obafemi Teminife <andysakov1958@gmail.com>
2023-10-12 04:27:49 +0100

    fix: fixed some bugs

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -41,7 +41,26 @@ export class JobsService {
     const generatedQuery = `
       MATCH (structured_jobpost:StructuredJobpost)-[:HAS_STATUS]->(:JobpostOnlineStatus)
       RETURN structured_jobpost {
-          .*,
+          id: structured_jobpost.id,
+          url: structured_jobpost.url,
+          title: structured_jobpost.title,
+          salary: structured_jobpost.salary,
+          culture: structured_jobpost.culture,
+          location: structured_jobpost.location,
+          summary: structured_jobpost.summary,
+          benefits: structured_jobpost.benefits,
+          shortUUID: structured_jobpost.shortUUID,
+          seniority: structured_jobpost.seniority,
+          description: structured_jobpost.description,
+          requirements: structured_jobpost.requirements,
+          paysInCrypto: structured_jobpost.paysInCrypto,
+          minimumSalary: structured_jobpost.minimumSalary,
+          maximumSalary: structured_jobpost.maximumSalary,
+          salaryCurrency: structured_jobpost.salaryCurrency,
+          responsibilities: structured_jobpost.responsibilities,
+          lastSeenTimestamp: structured_jobpost.lastSeenTimestamp,
+          firstSeenTimestamp: structured_jobpost.firstSeenTimestamp,
+          offersTokenAllocation: structured_jobpost.offersTokenAllocation,
           classification: [(structured_jobpost)-[:HAS_CLASSIFICATION]->(classification) | classification.name ][0],
           commitment: [(structured_jobpost)-[:HAS_COMMITMENT]->(commitment) | commitment.name ][0],
           locationType: [(structured_jobpost)-[:HAS_LOCATION_TYPE]->(locationType) | locationType.name ][0],
@@ -115,7 +134,26 @@ export class JobsService {
     const generatedQuery = `
           MATCH (structured_jobpost:StructuredJobpost)-[:HAS_STATUS]->(:JobpostOnlineStatus)
           RETURN structured_jobpost {
-              .*,
+              id: structured_jobpost.id,
+              url: structured_jobpost.url,
+              title: structured_jobpost.title,
+              salary: structured_jobpost.salary,
+              culture: structured_jobpost.culture,
+              location: structured_jobpost.location,
+              summary: structured_jobpost.summary,
+              benefits: structured_jobpost.benefits,
+              shortUUID: structured_jobpost.shortUUID,
+              seniority: structured_jobpost.seniority,
+              description: structured_jobpost.description,
+              requirements: structured_jobpost.requirements,
+              paysInCrypto: structured_jobpost.paysInCrypto,
+              minimumSalary: structured_jobpost.minimumSalary,
+              maximumSalary: structured_jobpost.maximumSalary,
+              salaryCurrency: structured_jobpost.salaryCurrency,
+              responsibilities: structured_jobpost.responsibilities,
+              lastSeenTimestamp: structured_jobpost.lastSeenTimestamp,
+              firstSeenTimestamp: structured_jobpost.firstSeenTimestamp,
+              offersTokenAllocation: structured_jobpost.offersTokenAllocation,
               classification: [(structured_jobpost)-[:HAS_CLASSIFICATION]->(classification) | classification.name ][0],
               commitment: [(structured_jobpost)-[:HAS_COMMITMENT]->(commitment) | commitment.name ][0],
               locationType: [(structured_jobpost)-[:HAS_LOCATION_TYPE]->(locationType) | locationType.name ][0],
@@ -495,7 +533,6 @@ export class JobsService {
     const results: AllJobsListResult[] = [];
 
     try {
-      this.logger.log("No cached jobs found, retrieving from db.");
       const orgJobs = await this.getAllJobsListResults();
       results.push(...orgJobs);
     } catch (err) {
