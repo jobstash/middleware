7e38e52c1bf65b57321b38c69afb2db92bb7f8b0
Obafemi Teminife <andysakov1958@gmail.com>
2023-10-15 19:43:18 +0100

    fix(user): fixed bug in user profile endpoint

--- src/shared/entities/user-profile.entity.ts
+++ src/shared/entities/user-profile.entity.ts
@@ -5,14 +5,13 @@ export class UserProfileEntity {
   constructor(private readonly raw: UserProfile) {}
 
   getProperties(): UserProfile {
-    const { contact } = this.raw;
     return new UserProfile({
       ...this.raw,
       avatar: notStringOrNull(this.raw.avatar),
       contact: {
-        ...contact,
-        value: notStringOrNull(contact?.value),
-        preferred: notStringOrNull(contact?.preferred),
+        ...this.raw?.contact,
+        value: notStringOrNull(this.raw?.contact?.value),
+        preferred: notStringOrNull(this.raw?.contact?.preferred),
       },
     });
   }
