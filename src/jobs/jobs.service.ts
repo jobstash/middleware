3f792b6ea22da1507370290a4946a212d0c050f5
Obafemi Teminife <andysakov1958@gmail.com>
2023-11-23 10:21:11 +0100

    fix(jobs): fix parsing of location filter

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -330,8 +330,7 @@ export class JobsService {
           organizationFilterList.includes(normalizeString(orgName))) &&
         (!seniorityFilterList ||
           seniorityFilterList.includes(normalizeString(seniority))) &&
-        (!locationFilterList ||
-          locationFilterList.includes(normalizeString(locationType))) &&
+        (!locationFilterList || locationFilterList.includes(locationType)) &&
         (!minHeadCount || (headcountEstimate ?? 0) >= minHeadCount) &&
         (!maxHeadCount || (headcountEstimate ?? 0) < maxHeadCount) &&
         (!minSalaryRange || (salary ?? 0) >= minSalaryRange) &&
