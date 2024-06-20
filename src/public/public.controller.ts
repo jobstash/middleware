ac2ec6bf5586a0e7ff4e75adcb2a37bbaaad36f7
duckdegen <98649644+duckdegen@users.noreply.github.com>
2024-06-20 22:03:17 +0200

    Update public.controller.ts to remove apibearerauth

--- src/public/public.controller.ts
+++ src/public/public.controller.ts
@@ -7,13 +7,12 @@ import {
 } from "@nestjs/common";
 import { PublicService } from "./public.service";
 import { ApiKeyGuard } from "src/auth/api-key.guard";
-import { ApiBearerAuth, ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
+import { ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
 import { JobListResult, PaginatedData } from "src/shared/interfaces";
 import { CustomLogger } from "src/shared/utils/custom-logger";
 import { AllJobsInput } from "./dto/all-jobs.input";
 import { ConfigService } from "@nestjs/config";
 
-@ApiBearerAuth()
 @Controller("public")
 export class PublicController {
   private readonly logger = new CustomLogger(PublicController.name);
