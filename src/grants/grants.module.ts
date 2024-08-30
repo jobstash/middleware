5ca7153607a0c389b8261ccbbe63cf5abaf1f1f6
Obafemi Teminife <andysakov1958@gmail.com>
2024-08-30 13:49:15 +0100

    fix(grants): fixed module bug

--- src/grants/grants.module.ts
+++ src/grants/grants.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { GrantsService } from "./grants.service";
 import { GrantsController } from "./grants.controller";
 import { GoogleBigQueryModule } from "src/google-bigquery/google-bigquery.module";
+import { MailService } from "src/mail/mail.service";
 
 @Module({
   imports: [GoogleBigQueryModule],
   controllers: [GrantsController],
-  providers: [GrantsService],
+  providers: [GrantsService, MailService],
 })
 export class GrantsModule {}
