953423ace1473ecbdb9ae300726fe0b85bc3bb0d
Obafemi Teminife <andysakov1958@gmail.com>
2024-10-10 10:03:31 +0100

    fix(profile): fixed bug that allows multiple users to own one gh acct

--- src/auth/privy/privy.service.ts
+++ src/auth/privy/privy.service.ts
@@ -15,7 +15,7 @@ export class PrivyService {
     );
   }
 
-  async getUser(userId: string, attempts = 0): Promise<User> {
+  async getUser(userId: string, attempts = 0): Promise<User | undefined> {
     let user: User;
     try {
       user = await this.privy.getUser(userId);
@@ -30,22 +30,26 @@ export class PrivyService {
         );
       }
     } catch (err) {
-      Sentry.withScope(scope => {
-        scope.setTags({
-          action: "service-call",
-          source: "privy.service",
+      if (err.message === "User not found") {
+        return undefined;
+      } else {
+        Sentry.withScope(scope => {
+          scope.setTags({
+            action: "service-call",
+            source: "privy.service",
+          });
+          Sentry.captureException(err);
         });
-        Sentry.captureException(err);
-      });
-      this.logger.error(`PrivyService::getUser ${err.message}`);
-      const backOffTime = Math.min(360000, Math.pow(2, attempts) * 1000); // Cap back-off time to 60 seconds
-      this.logger.warn(
-        `Rate limited on get user request. Retrying after ${
-          backOffTime / 1000
-        } seconds...`,
-      );
-      await new Promise(resolve => setTimeout(resolve, backOffTime));
-      return this.getUser(userId, attempts + 1);
+        this.logger.error(`PrivyService::getUser ${err.message}`);
+        const backOffTime = Math.min(360000, Math.pow(2, attempts) * 1000); // Cap back-off time to 60 seconds
+        this.logger.warn(
+          `Rate limited on get user request. Retrying after ${
+            backOffTime / 1000
+          } seconds...`,
+        );
+        await new Promise(resolve => setTimeout(resolve, backOffTime));
+        return this.getUser(userId, attempts + 1);
+      }
     }
     return user;
   }
--- src/auth/profile/profile.service.ts
+++ src/auth/profile/profile.service.ts
@@ -632,20 +632,21 @@ export class ProfileService {
       const privyId = await this.getPrivyId(wallet);
       const user = await this.privyService.getUser(privyId);
       if (
-        profile?.linkedAccounts.github !== user.github?.username ||
-        (profile.linkedAccounts.github && !user.github)
+        profile?.linkedAccounts.github &&
+        (profile?.linkedAccounts.github !== dto.github || !dto.github)
       ) {
         this.logger.log(
           `Unlinking github user ${profile?.linkedAccounts.github}`,
         );
         await this.githubUserService.removeGithubInfoFromUser(wallet);
       }
-      if (user.github) {
-        this.logger.log(`Fetching github info for ${user.github.username}`);
+
+      if (dto.github && profile?.linkedAccounts.github !== dto.github) {
+        this.logger.log(`Fetching github info for ${dto.github}`);
         const githubUser = axios
           .get<{
             avatar_url: string;
-          }>(`https://api.github.com/users/${user.github.username}`)
+          }>(`https://api.github.com/users/${dto.github}`)
           .catch(err => {
             this.logger.error(`UserService::fetchGithubUser ${err.message}`);
             this.logger.error(err);
@@ -661,7 +662,7 @@ export class ProfileService {
 
         const result = await this.githubUserService.addGithubInfoToUser({
           wallet,
-          githubLogin: user.github.username,
+          githubLogin: dto.github,
           githubId: user.github.subject,
           githubAvatarUrl: (await githubUser)?.data?.avatar_url ?? null,
         });
