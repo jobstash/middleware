6649b8261f6b6644d77a9d6e1b3841bff2bce8b9
DuckDegen <duckdegen@gmail.com>
2025-03-07 17:23:22 +0800

    block shit

--- src/search/search.guard.ts
+++ src/search/search.guard.ts
@@ -15,7 +15,7 @@ export class BlockScrapersGuard implements CanActivate {
 
     // Check the user agent.
     const userAgent = request.headers["user-agent"];
-    if (userAgent && userAgent.toLowerCase() === "1node") {
+    if (userAgent && userAgent.toLowerCase() === "node") {
       throw new ForbiddenException(
         "WTF are you doing man? 🤨 Reach out to @duckdegen and lets discuss",
       );
