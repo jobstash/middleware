f1b7e084dd1733117867fbb7a611fc33cb7acfee
Obafemi Teminife <andysakov1958@gmail.com>
2025-03-24 12:01:21 +0100

    feat(subs): added get sub usage summary endpoint

--- src/subscriptions/subscriptions.controller.ts
+++ src/subscriptions/subscriptions.controller.ts
@@ -13,7 +13,12 @@ import { CustomLogger } from "src/shared/utils/custom-logger";
 import { PBACGuard } from "src/auth/pbac.guard";
 import { CheckWalletPermissions } from "src/shared/constants";
 import { Permissions, Session } from "src/shared/decorators";
-import { Subscription, Payment, QuotaUsage } from "src/shared/interfaces/org";
+import {
+  Subscription,
+  Payment,
+  QuotaUsage,
+  MeteredService,
+} from "src/shared/interfaces/org";
 import {
   data,
   ResponseWithOptionalData,
@@ -22,6 +27,7 @@ import {
 import { UserService } from "src/user/user.service";
 import { NewSubscriptionInput } from "./new-subscription.input";
 import { now } from "lodash";
+import { subMonths } from "date-fns";
 
 @Controller("subscriptions")
 export class SubscriptionsController {
@@ -78,7 +84,7 @@ export class SubscriptionsController {
   @Get(":orgId/usage")
   @UseGuards(PBACGuard)
   @Permissions(CheckWalletPermissions.USER, CheckWalletPermissions.ORG_OWNER)
-  async getOrgUsage(
+  async getOrgUsageData(
     @Param("orgId") orgId: string,
     @Session() { address }: SessionObject,
     @Query("epochStart") epochStart?: number,
@@ -111,6 +117,77 @@ export class SubscriptionsController {
           success: true,
           message: "Retrieved organization usage",
           data: usage,
+        };
+      }
+    } else {
+      throw new UnauthorizedException({
+        success: false,
+        message: "You are not the owner of this organization",
+      });
+    }
+  }
+
+  @Get(":orgId/usage/summary")
+  @UseGuards(PBACGuard)
+  @Permissions(CheckWalletPermissions.USER, CheckWalletPermissions.ORG_OWNER)
+  async getOrgUsageSummary(
+    @Param("orgId") orgId: string,
+    @Session() { address }: SessionObject,
+    @Query("epochStart") epochStart?: number,
+    @Query("epochEnd") epochEnd?: number,
+  ): Promise<
+    ResponseWithOptionalData<
+      {
+        service: string;
+        totalUsage: number;
+        availableCredits: number;
+      }[]
+    >
+  > {
+    this.logger.log(`/subscriptions/${orgId}/usage/summary ${address}`);
+
+    const owner = data(
+      await this.userService.findOrgOwnerProfileByOrgId(orgId),
+    );
+    if (owner?.wallet === address) {
+      const subscription = data(
+        await this.subscriptionsService.getSubscriptionInfo(orgId),
+      );
+      if (subscription) {
+        let usage: Map<MeteredService, number>;
+        let availableCredits: Map<MeteredService, number>;
+        if (epochStart && epochEnd) {
+          usage = subscription.getEpochAggregateUsage(epochStart, epochEnd);
+          availableCredits = subscription.getEpochAggregateAvailableCredits(
+            epochStart,
+            epochEnd,
+          );
+        } else if (epochStart && !epochEnd) {
+          usage = subscription.getEpochAggregateUsage(epochStart, now());
+          availableCredits = subscription.getEpochAggregateAvailableCredits(
+            epochStart,
+            now(),
+          );
+        } else if (!epochStart && epochEnd) {
+          return {
+            success: false,
+            message: "Must specify epochStart if epochEnd is specified",
+          };
+        } else {
+          usage = subscription.getCurrentEpochAggregateUsage();
+          availableCredits = subscription.getEpochAggregateAvailableCredits(
+            subMonths(subscription.expiryTimestamp, 2).getTime(),
+            now(),
+          );
+        }
+        return {
+          success: true,
+          message: "Retrieved organization usage",
+          data: Array.from(usage).map(x => ({
+            service: x[0],
+            totalUsage: x[1],
+            availableCredits: availableCredits.get(x[0]) ?? 0,
+          })),
         };
       }
     } else {
