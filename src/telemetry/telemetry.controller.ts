732b9aa9a78ffc76d102c37afe5922e3cbb1dd25
Obafemi Teminife <andysakov1958@gmail.com>
2025-04-03 12:39:04 +0100

    fix(telemetry): fixed org membership access control for accessing telemetry data

--- src/telemetry/telemetry.controller.ts
+++ src/telemetry/telemetry.controller.ts
@@ -9,26 +9,37 @@ import {
 import { TelemetryService } from "./telemetry.service";
 import { PBACGuard } from "src/auth/pbac.guard";
 import { CACHE_DURATION, CheckWalletPermissions } from "src/shared/constants";
-import { Permissions } from "src/shared/decorators";
-import { ResponseWithOptionalData } from "src/shared/interfaces";
+import { Permissions, Session } from "src/shared/decorators";
+import { ResponseWithOptionalData, SessionObject } from "src/shared/interfaces";
 import { GetJobStatsInput } from "./dto/get-job-stats.input";
 import { CustomLogger } from "src/shared/utils/custom-logger";
 import { CacheHeaderInterceptor } from "src/shared/decorators/cache-interceptor.decorator";
+import { UserService } from "src/user/user.service";
 
 @Controller("telemetry")
 export class TelemetryController {
   private logger = new CustomLogger(TelemetryController.name);
-  constructor(private readonly telemetryService: TelemetryService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly telemetryService: TelemetryService,
+  ) {}
 
   @Get("job/views")
   @UseGuards(PBACGuard)
   @Permissions(CheckWalletPermissions.USER, CheckWalletPermissions.ORG_MEMBER)
   @UseInterceptors(new CacheHeaderInterceptor(CACHE_DURATION))
   async getJobViewCount(
+    @Session() { address }: SessionObject,
     @Query(new ValidationPipe({ transform: true }))
     params: GetJobStatsInput,
   ): Promise<ResponseWithOptionalData<number>> {
     this.logger.log(`/telemetry/job/views ${JSON.stringify(params)}`);
+    if (!(await this.userService.isOrgMember(address, params.orgId))) {
+      return {
+        success: false,
+        message: "You are not authorized to access this resource",
+      };
+    }
     return this.telemetryService.getJobViewCount(params);
   }
 
@@ -37,10 +48,17 @@ export class TelemetryController {
   @Permissions(CheckWalletPermissions.USER, CheckWalletPermissions.ORG_MEMBER)
   @UseInterceptors(new CacheHeaderInterceptor(CACHE_DURATION))
   async getJobApplyCount(
+    @Session() { address }: SessionObject,
     @Query(new ValidationPipe({ transform: true }))
     params: GetJobStatsInput,
   ): Promise<ResponseWithOptionalData<number>> {
     this.logger.log(`/telemetry/job/applies ${JSON.stringify(params)}`);
+    if (!(await this.userService.isOrgMember(address, params.orgId))) {
+      return {
+        success: false,
+        message: "You are not authorized to access this resource",
+      };
+    }
     return this.telemetryService.getJobApplyCount(params);
   }
 }
--- src/telemetry/telemetry.module.ts
+++ src/telemetry/telemetry.module.ts
@@ -2,9 +2,10 @@ import { forwardRef, Module } from "@nestjs/common";
 import { TelemetryService } from "./telemetry.service";
 import { TelemetryController } from "./telemetry.controller";
 import { AuthModule } from "src/auth/auth.module";
+import { UserModule } from "src/user/user.module";
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), forwardRef(() => UserModule)],
   controllers: [TelemetryController],
   providers: [TelemetryService],
   exports: [TelemetryService],
