8e2cc71eeb81fb5e6144657b3835d7f7d24430f3
Obafemi Teminife <andysakov1958@gmail.com>
2025-04-15 13:22:13 +0100

    fix(projects): fixed validation bug on project details endpoint

--- src/shared/entities/project-details.entity.ts
+++ src/shared/entities/project-details.entity.ts
@@ -8,6 +8,7 @@ import {
 import { ProjectDetailsResult } from "../interfaces/project-details-result.interface";
 import { OrgReviewEntity } from "./org-review.entity";
 import { uniqBy } from "lodash";
+import { isAfter, isBefore } from "date-fns";
 
 type RawProject = ProjectDetailsResult & {
   organization?: (OrganizationWithRelations & { tags: Tag[] }) | null;
@@ -153,6 +154,44 @@ export class ProjectDetailsEntity {
           roundName: notStringOrNull(fr?.roundName),
           sourceLink: notStringOrNull(fr?.sourceLink),
         })) ?? [],
+      jobs:
+        project?.jobs.map(jobpost => {
+          const now = new Date().getTime();
+
+          const isStillFeatured =
+            jobpost?.featured === true &&
+            isAfter(now, nonZeroOrNull(jobpost?.featureStartDate) ?? now) &&
+            isBefore(now, nonZeroOrNull(jobpost?.featureEndDate) ?? now);
+
+          return {
+            ...jobpost,
+            salary: nonZeroOrNull(jobpost?.salary),
+            minimumSalary: nonZeroOrNull(jobpost?.minimumSalary),
+            maximumSalary: nonZeroOrNull(jobpost?.maximumSalary),
+            seniority: notStringOrNull(jobpost?.seniority, ["", "undefined"]),
+            culture: notStringOrNull(jobpost?.culture, ["", "undefined"]),
+            salaryCurrency: notStringOrNull(jobpost?.salaryCurrency),
+            paysInCrypto: jobpost?.paysInCrypto ?? null,
+            offersTokenAllocation: jobpost?.offersTokenAllocation ?? null,
+            url:
+              jobpost?.access === "protected"
+                ? null
+                : notStringOrNull(jobpost?.url),
+            title: notStringOrNull(jobpost?.title),
+            summary: notStringOrNull(jobpost?.summary),
+            description: notStringOrNull(jobpost?.description),
+            commitment: notStringOrNull(jobpost?.commitment),
+            timestamp: nonZeroOrNull(jobpost?.timestamp),
+            featureStartDate: isStillFeatured
+              ? nonZeroOrNull(jobpost?.featureStartDate)
+              : null,
+            featureEndDate: isStillFeatured
+              ? nonZeroOrNull(jobpost?.featureEndDate)
+              : null,
+            featured: isStillFeatured,
+            onboardIntoWeb3: jobpost?.onboardIntoWeb3 ?? false,
+          };
+        }) ?? [],
     });
   }
 }
