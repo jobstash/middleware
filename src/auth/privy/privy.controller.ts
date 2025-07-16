7a00fc696bcfdb68e37637ff0bb2d2e3cecef682
Obafemi Teminife <andysakov1958@gmail.com>
2025-07-16 14:29:25 +0100

    fix(privy): fixed webhook bug

--- src/auth/privy/privy.controller.ts
+++ src/auth/privy/privy.controller.ts
@@ -151,22 +151,6 @@ export class PrivyController {
           }
         } else {
           this.logger.warn(`User not found`);
-          const embeddedWallet = await this.privyService.getUserEmbeddedWallet(
-            (verifiedPayload as PrivyUpdateEventPayload).user.id,
-          );
-          if (embeddedWallet) {
-            this.logger.log(`User created: ${embeddedWallet}`);
-            await this.userService.syncUserLinkedWallets(
-              embeddedWallet,
-              (verifiedPayload as PrivyUpdateEventPayload).user,
-            );
-            await this.userService.updateLinkedAccounts(
-              verifiedPayload as PrivyUpdateEventPayload,
-              embeddedWallet,
-            );
-          } else {
-            this.logger.warn(`User not found`);
-          }
         }
       } else if (verifiedPayload.type === "user.transferred_account") {
         const payload = verifiedPayload as PrivyTransferEventPayload;
@@ -190,7 +174,7 @@ export class PrivyController {
         }
       } else if (verifiedPayload.type === "user.created") {
         const payload = verifiedPayload as PrivyCreateEventPayload;
-        const embeddedWallet = await this.privyService.getUserEmbeddedWallet(
+        const embeddedWallet = await this.userService.getEmbeddedWallet(
           payload.user.id,
         );
         if (embeddedWallet) {
